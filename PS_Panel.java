1f04e98d15a7cd2e3af12983bf71839e27e0cb98
neilrawlins <rawly50@yahoo.com>
2016-04-25 20:46:19 -0400

    Added files via upload

---
+++ PS_Panel.java
@@ -0,0 +1,39 @@
+package prisonsyncgitversion;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D; 
+import java.awt.Image;
+import java.io.File;
+import java.io.IOException;
+import java.util.ConcurrentModificationException;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+public class PS_Panel extends JPanel {
+	//declare local variables
+	private Image image;
+	
+	/**
+	 * Create a jpanel with a background image
+	 */
+	public PS_Panel() {
+		try {
+			image = ImageIO.read(new File("PrisonBars.jpg"));
+		} catch (IOException e) {
+			System.out.println("Error trying to access image.");
+		}		
+	}
+	/*
+	 * allows panels of this type to have background images
+	 * by drawing the image using paint component
+	 */
+	//public void paintComponent(Graphics g) {
+		//super.paintComponent(g);
+		//Graphics2D g2 = (Graphics2D) g;
+
+		//draw the background image
+		//image = image.getScaledInstance(this.getWidth(), this.getHeight(), 0);
+		//g2.drawImage(image, 0,0, null);
+	//}
+}
